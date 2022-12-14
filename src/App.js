e448960970279520788ef58150137cd8e14bf8d8
Ericledev <lehuutuan368@gmail.com>
2022-12-14 20:29:47 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./App.css";
+import Layout from "./components/layout/Layout";
+import CartPage from "./components/pages/CartPage";
+import CheckoutPage from "./components/pages/CheckoutPage";
+import DetailPage from "./components/pages/DetailPage";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
+import ShopPage from "./components/pages/ShopPage";
 
 function App() {
+  const { isLogin } = useSelector((state) => state.logInReducer);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/*" element={<HomePage />} />
+        <Route path="/login" element={!isLogin && <LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/detail/:productId" element={<DetailPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
